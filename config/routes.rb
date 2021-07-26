11206acce6074664c5e651c3e67d8fa88db2236c
syinGit <syin1@chicagobooth.edu>
2021-07-26 08:49:53 +0000

    msm-queries (video complete)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  #static routes should be above dynamic routes, else all will be routed to first route (sequence matters!)
+
+  get("/directors/eldest", { :controller => "directors", :action => "wisest"})
+
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details"})
+
   get("/", { :controller => "application", :action => "homepage" })
+
+  get("/directors", { :controller => "directors", :action => "index"})
+
 end
